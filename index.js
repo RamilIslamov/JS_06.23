d835c496ecaa94f1d9420069004c357622085c7a
ramil.islamov.88 <ramil.islamov.88@mail.ru>
2023-06-08 16:08:58 +0400

    homework_3 is done

--- index.js
+++ index.js
@@ -1,123 +1,67 @@
-function makeDeepCopy(obj) {
-  const prohibitedTypes = ['number', 'string', 'boolean', 'symbol', 'bigint'];
-  const type = typeof obj;
-  if (Array.isArray(obj) || obj == null || prohibitedTypes.includes(type)) {
-    throw new Error();
-  }
-  const copy = {};
+Array.prototype.customFilter = customFilter;
 
-  function makeDeepCopyArray(arr) {
-    const copyArr = arr.map((element) => {
-      if (Array.isArray(element)) {
-        return makeDeepCopyArray(element);
-      }
-      if (typeof element === 'object' && !Array.isArray(element)) {
-        return makeDeepCopy(element);
-      } else {
-        return element;
-      }
-    });
-
-    return copyArr;
+function customFilter(callBack, thisArg) {
+  if (
+    typeof callBack !== 'function' ||
+    (thisArg && typeof thisArg !== 'object')
+  ) {
+    throw new Error('Invalid argument.');
   }
 
-  for (let prop in obj) {
-    if (Array.isArray(obj[prop])) {
-      copy[prop] = makeDeepCopyArray(obj[prop]);
-    } else if (obj[prop] instanceof Map) {
-      copy[prop] = new Map();
-      for (let [key, value] of obj[prop]) {
-        if (typeof key === 'object' || (Array.isArray(key) && key !== null)) {
-          copy[prop].set(makeDeepCopy(key), value);
-        } else {
-          copy[prop].set(key, value);
-        }
-      }
-    } else if (obj[prop] instanceof Set) {
-      copy[prop] = new Set([...obj[prop]]);
-    } else if (
-      typeof obj[prop] === 'object' &&
-      obj[prop] !== null &&
-      !Array.isArray(obj[prop])
-    ) {
-      copy[prop] = makeDeepCopy(obj[prop]);
-    } else {
-      copy[prop] = obj[prop];
+  const filteredArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callBack.call(thisArg, this[i], i, this)) {
+      filteredArray.push(this[i]);
     }
   }
-  return copy;
+  return filteredArray;
 }
 
-function createIterable(from, to) {
-  if (
-    isNaN(from) ||
-    isNaN(to) ||
-    !isFinite(from) ||
-    !isFinite(to) ||
-    !Number.isInteger(from) ||
-    !Number.isInteger(to) ||
-    typeof from !== 'number' ||
-    typeof to !== 'number' ||
-    to <= from
-  ) {
-    throw new Error();
+function bubbleSort(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('Invalid argument.');
   }
-  let currentVal = from;
+  if (arr.length === 0) {
+    return [];
+  }
+  const newArr = [...arr];
 
-  return {
-    [Symbol.iterator]: function () {
-      return {
-        next() {
-          if (currentVal <= to) {
-            return { done: false, value: currentVal++ };
-          } else {
-            return { done: true };
-          }
-        },
-      };
-    },
-  };
+  for (let i = 0; i < newArr.length; i++) {
+    if (typeof newArr[i] !== 'number' || !isFinite(newArr[i])) {
+      throw new Error('Invalid argument.');
+    }
+  }
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < newArr.length; i++) {
+      if (newArr[i + 1] < newArr[i]) {
+        [newArr[i], newArr[i + 1]] = [newArr[i + 1], newArr[i]];
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return newArr;
 }
 
-function createProxy(obj) {
-  if (typeof obj !== 'object' || Array.isArray(obj) || obj == null) {
-    throw new Error();
+function storageWrapper(callBackFn, arr) {
+  if (typeof callBackFn !== 'function') {
+    throw new Error('Invalid argument.');
   }
+  if (!Array.isArray(arr) && arr !== undefined) {
+    throw new Error('Invalid argument.');
+  }
+  const storage = [];
 
-  const objProxyHandler = {
-    get: function (target, prop) {
-      if (prop in target) {
-        if (target[prop] && typeof target[prop] === 'object') {
-          target[prop].readAmount = (target[prop].readAmount || 0) + 1;
-        } else {
-          target[prop] = {
-            value: target[prop],
-            readAmount: 1,
-          };
-        }
-      }
-      return target[prop];
-    },
-    set: function (target, prop, val) {
-      if (!(prop in target)) {
-        target[prop] = {
-          value: val,
-          readAmount: 0,
-        };
-        return true;
-      }
-      if (target[prop] && typeof target[prop] === 'object') {
-        if (typeof val === typeof target[prop].value) {
-          target[prop].value = val;
-        } else {
-          if (typeof val === typeof target[prop]) {
-            target[prop] = { ...target[prop], value: val, readAmount: 0 };
-          }
-        }
-      }
-      return true;
-    },
+  return function () {
+    if (arr === undefined) {
+      const res = callBackFn();
+      storage.push(res);
+      return storage;
+    } else {
+      const res = callBackFn();
+      arr.push(res);
+      return arr;
+    }
   };
-
-  return new Proxy(obj, objProxyHandler);
 }
